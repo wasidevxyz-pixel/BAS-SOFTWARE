2d81c46f16d734680692252909f22c7461b21bbe
Itxxwasi <somkerboy061@gmail.com>
2025-12-25 20:12:01 +0500

    updated defualt port

--- Backend/server.js
+++ Backend/server.js
@@ -562,7 +562,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ success: false, message: err && err.message ? err.message : 'Something went wrong!' });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const HOST = '0.0.0.0';
 const server = app.listen(PORT, HOST, () => {
   console.log(`Server is running on ${HOST}:${PORT}`);
