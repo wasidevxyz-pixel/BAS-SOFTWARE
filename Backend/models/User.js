99c7dde3b69902383f3dc6e831ed0135bc173314
wasidevxyz-pixel <wasi.devxyz@gmail.com>
2026-02-05 14:32:59 +0500

    Improvement: Added trim and lowercase to Login ID (email) to ensure login consistency

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -11,7 +11,9 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, 'Please provide a login id'],
-    unique: true
+    unique: true,
+    trim: true,
+    lowercase: true
   },
   password: {
     type: String,
